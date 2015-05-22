a462a5c26749bcfb868e85e0520fb7d4de8b2ec7
antoine vastel <antoine.vastel@gmail.com>
2015-05-22 13:51:07 +0200

    Changed the ui for global stats page

--- website/public/javascripts/stats.js
+++ website/public/javascripts/stats.js
@@ -1,10 +1,55 @@
+function sendDates(datel, dateu, typeRequest){
+	var dates = {};
+	dates.datel = datel;
+	dates.dateu = dateu;
+
+	$("body").append('<form style="visibility:hidden" id="fakeForm" aaction="/stats" method="POST"><input type="date"  value="'+datel+'" name="datel" /><input type="date"  value="'+dateu+'" name="dateu"/><input type="text" value="'+typeRequest+'" name="typereq"><button id="valid" type="submit" class="btn btn-default">Envoyer</button>');
+
+    $('#valid').trigger('click');
+	/*
+	$.ajax({
+	    url: '/stats',
+	    data: dates, 
+	    contentType: 'application/x-www-form-urlencoded', 
+	    method: "POST",
+	    success: function(data){
+	    	
+	    	//regexp to get the body
+		   	var body=data.replace(/^[\S\s]*<body[^>]*?>/i, "")
+	                    .replace(/<\/body[\S\s]*$/i, "");
+	    	$("body").html(body);
+	    	$("head").html("");
+	    	
+	    }
+  	});
+	*/
+}
+
 $( document ).ready(function() {
+    $("a[role=button]").click(1000,function(){
+        if( $(this).attr("id") != "custom"){
+            if($(this).attr("id") === "all"){
+            	//Call the page without any parameters
+            	window.location.href = "/stats";
+            }else if($(this).attr("id") === "month"){
+            	datel = new Date().last().month().toString('yyyy-MM-dd');
+            	dateu = Date.today().toString('yyyy-MM-dd') ;
+            	sendDates(datel, dateu, "month");
+            }else if($(this).attr("id") === "week"){
+            	datel = new Date().last().week().toString('yyyy-MM-dd');
+            	dateu = Date.today().toString('yyyy-MM-dd') ;
+            	sendDates(datel, dateu, "week");
+            }
+        }else if($(this).attr("id") === "custom"){
+            $("#dateForm").css("display","block");
+        }
+    });
+
     $('#validate').click(function(e){
     	datel = $("#datel").val();
     	dateu = $("#dateu").val();
-
       //We check if the date has the good format
-      if(!(date1.match(/^[0-9]{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/) && dateu.match(/^[0-9]{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/))
+      if(!(date1.match(/^[0-9]{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/) && dateu.match(/^[0-9]{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/)))
       {
         e.preventDefault();
       }
