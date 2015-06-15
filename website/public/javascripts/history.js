f9d05d51c801885821e06f463f017dcf81c7dc53
Pierre Laperdrix <pierre.laperdrix@inria.fr>
2015-06-15 17:27:20 +0200

    Fixing missing files from previous commit

--- website/public/javascripts/history.js
+++ website/public/javascripts/history.js
@@ -76,6 +76,10 @@ $( document ).ready(function() {
         }else{                                                                                                                                                                                                                                        
             var vals = getCheckedValues();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
             var counters = {};
+            var nbLoaded = 0;
+            var canvas1 = document.createElement("canvas");
+            var canvas2 = document.createElement("canvas");
+
             counters.list = vals;
             $.ajax({                                                                                                                                                                                                
                 url: '/compareFpHistory',
@@ -106,7 +110,7 @@ $( document ).ready(function() {
                           var pluginsParsedBefore = colBefore.split("Plugin ");
                           var pluginsParsedAfter = colAfter.split("Plugin ");
 
-                          var pluginsBefore = new Array(); 
+                          var pluginsBefore = new Array();
                           for(var i = 1; i < pluginsParsedBefore.length; i++){
                             pluginsBefore[i-1] = pluginsParsedBefore[i].split(": ")[1]+' ';
                           }
@@ -133,23 +137,64 @@ $( document ).ready(function() {
                         var lineText1 = a['chars1'];
                         var lineText2 = a['chars2'];
                         var lineArray = a['lineArray'];
-                         
-                        diff = dmp.diff_main(lineText1, lineText2, false);
-                        dmp.diff_charsToLines_(diff, lineArray);
 
-
-                        if(colBefore==="Flash not detected"){
+                        if(colAttribute != "canvasJs") {
+                            diff = dmp.diff_main(lineText1, lineText2, false);
+                            dmp.diff_charsToLines_(diff, lineArray);
+                            if(colBefore==="Flash not detected"){
                             $row.find(':nth-child(4)').html('<del style="background:#ffe6e6;">Flash not detected </del><ins style="background:#e6ffe6;">'+colAfter+'</ins>');
-                        }else if(colAfter==="Flash not detected"){
-                            $row.find(':nth-child(4)').html('<ins style="background:#e6ffe6;">Flash not detected </ins><del style="background:#ffe6e6;">'+colBefore+'</del>');
-                        }else{
-                            $row.find(':nth-child(4)').html(dmp.diff_prettyHtml(diff));
+                            }else if(colAfter==="Flash not detected"){
+                                $row.find(':nth-child(4)').html('<ins style="background:#e6ffe6;">Flash not detected </ins><del style="background:#ffe6e6;">'+colBefore+'</del>');
+                            }else{
+                                $row.find(':nth-child(4)').html(dmp.diff_prettyHtml(diff));
+                            }
+                        } else {
+                            var img1 = document.getElementById("img1");
+                            var img2 = document.getElementById("img2");
+
+                            canvas1.id = "cvs1";
+                            canvas1.width = img1.width;
+                            canvas1.height = img1.height;
+                            ctx1 = canvas1.getContext("2d");
+                            imgCvs1 = new Image();
+                            imgCvs1.onload = function() {
+                                ctx1.drawImage(imgCvs1, 0, 0);
+                                nbLoaded++;
+                            };
+                            imgCvs1.src = img1.src;
+
+                            canvas2.id = "cvs2";
+                            canvas2.width = img1.width;
+                            canvas2.height = img1.height;
+                            ctx2 = canvas2.getContext("2d");
+                            imgCvs2 = new Image();
+                            imgCvs2.onload = function() {
+                                ctx2.drawImage(imgCvs2, 0, 0);
+                                nbLoaded++;
+                            };
+                            imgCvs2.src = img2.src;
+
+                            finishCallback();
                         }
 
                     });
                                             
                 }
             });
+
+            function finishCallback(){
+                console.log("Début du callback");
+                if (nbLoaded < 2) {
+                    console.log(nbLoaded);
+                    setTimeout(function() {
+                        finishCallback();
+                    }, 100);
+                } else {
+                    var diffImg = document.getElementById("diffImg");
+                    var canvasDiff = compareCanvas(canvas1, canvas2);
+                    diffImg.src= canvasDiff.toDataURL();;
+                }
+            }
         }
     });
 });
