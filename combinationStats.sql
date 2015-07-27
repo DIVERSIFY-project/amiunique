9a8b789e58c736294b694a6d54271a0c1d81ed98
Pierre Laperdrix <pierre.laperdrix@inria.fr>
2015-07-27 10:10:06 +0200

    Updating structure of two SQL tables

--- combinationStats.sql
+++ combinationStats.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.4.11
 -- http://www.phpmyadmin.net
 --
--- Client: localhost
--- Généré le: Jeu 21 Mai 2015 à 17:40
--- Version du serveur: 5.5.43-0ubuntu0.14.04.1
--- Version de PHP: 5.5.9-1ubuntu4.9
+-- Host: localhost
+-- Generation Time: Jul 27, 2015 at 08:06 AM
+-- Server version: 10.0.20-MariaDB
+-- PHP Version: 5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,27 +14,44 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données: `fingerprint`
+-- Database: `fingerprint`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `combinationStats`
+-- Table structure for table `combinationStats`
 --
 
 CREATE TABLE IF NOT EXISTS `combinationStats` (
-  `counter` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `counter` int(10) unsigned NOT NULL,
   `combination` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   `indicator` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-  `number` int(11) NOT NULL,
-  `percentage` float NOT NULL,
-  PRIMARY KEY (`counter`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `combinationStats`
+--
+ALTER TABLE `combinationStats`
+  ADD PRIMARY KEY (`counter`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `combinationStats`
+--
+ALTER TABLE `combinationStats`
+  MODIFY `counter` int(10) unsigned NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
